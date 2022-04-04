4904e12568ecb092ff309d6e1bac02bb854c714a
Deyvison Costa <deyvisongtr@gmail.com>
2022-04-04 03:13:32 -0300

    Final commit settings redux-saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,25 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga"
 import { ICartState } from "./modules/cart/types";
 import rootReducer from "./modules/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootSaga from "./modules/rootSaga";
 
 export interface IState {
   cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware()
+
+const middleware = [sagaMiddleware]
+
 //Provider
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+  ));
+
+  sagaMiddleware.run(rootSaga)
 
 export default store;
